d6517496b8117292bc6d3bb0f3001a175c8c2ffa
jhw <unity3013@dkitec.com>
2023-12-20 18:33:10 +0900

    Project Setup 2

---
+++ tip-calculator/view/SplitInputView.swift
@@ -0,0 +1,24 @@
+//
+//  SplitInputView.swift
+//  tip-calculator
+//
+//  Created by Hyunwoo Jeon on 1/15/24.
+//
+
+import UIKit
+
+class SplitInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemBlue
+    }
+}
