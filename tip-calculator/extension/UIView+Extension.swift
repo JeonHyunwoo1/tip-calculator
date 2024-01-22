9ce9aae25162e3d7d2d99e1d3e8a82998c947f7e
jhw <unity3013@dkitec.com>
2024-01-22 17:00:17 +0900

    add ResultView

---
+++ tip-calculator/extension/UIView+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIView+Extension.swift
+//  tip-calculator
+//
+//  Created by Hyunwoo Jeon on 1/22/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+        
+        
+        
+    }
+}
