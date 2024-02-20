36bb00a20a25b40711044276840237c1c99b10cb
jhw <unity3013@dkitec.com>
2024-02-20 17:13:50 +0900

    - Add view model - Define the inputs and outputs

---
+++ tip-calculator/model/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Hyunwoo Jeon on 2/20/24.
+//
+
+import Foundation
---
+++ tip-calculator/view model/CalculatorVM.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorVM.swift
+//  tip-calculator
+//
+//  Created by Hyunwoo Jeon on 2/14/24.
+//
+
+import Foundation
